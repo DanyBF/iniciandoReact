70e15a1cd4f9e8b8e86b5de4163daf8e0d58e3bb
Dany BF <daniela.dbf95@gmail.com>
2023-09-28 23:09:04 -0300

    twfollowcard

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,7 +1,12 @@
-export function App (){
- return (
-  <div>
-    <h1>Twitter Card</h1>
-  </div>
- ) 
+import './app.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+export function App() {
+  return (
+    <>    
+    <section className="App">
+    <TwitterFollowCard userName='midudev' name="Miguel Angel Duran" />
+    <TwitterFollowCard userName='danybf' name="Daniela Belen Fernandez" />
+    </section>
+    </>
+  )
 }
