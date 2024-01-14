481dece46515797dbad47c344c75eac7f5caa4ad
Dany BF <daniela.dbf95@gmail.com>
2024-01-14 20:28:27 -0300

    final update

--- projects/01-twitter-card/src/App.jsx
+++ projects/01-twitter-card/src/App.jsx
@@ -3,41 +3,39 @@ import { useState } from 'react'
 import './app.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
-
 const users = [
   {
-    userName:'midudev',
-    name:'Miguel Angel Duran',
-    isFollowing:true
+    userName: 'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: true,
   },
   {
-    userName:'danybf',
-    name:'Daniela Belen Fernandez',
-    isFollowing:true
+    userName: 'danydbf',
+    name: 'Daniela Belen Fernandez',
+    isFollowing: true,
   },
   {
-    userName:'zahifernandez_',
-    name:'Zahira Fernandez',
-    isFollowing:false
-  }
+    userName: 'zahifernandez_',
+    name: 'Zahira Fernandez',
+    isFollowing: false,
+  },
 ]
 
 export function App() {
   return (
-    <section className="App">
-      {
-        users.map(user =>{
-          const {userName, name, isFollowing} = user
-          return(
-            <TwitterFollowCard
+    <section className='App'>
+      {users.map((user) => {
+        const { userName, name, isFollowing } = user
+        return (
+          <TwitterFollowCard
             key={userName}
             userName={userName}
-            initialIsFollowing={isFollowing}>
-              {name}
-            </TwitterFollowCard>
-          )
-        })
-      }
-   </section>
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        )
+      })}
+    </section>
   )
-} 
+}
