3679a1ba31add06ac599530e585387b026afde22
Dany BF <daniela.dbf95@gmail.com>
2024-01-16 23:03:32 -0300

    init

--- projects/02-conecta-4/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-import './App.css'
-import { Board } from './components/Board'
-import { Slot } from './components/Slot'
-
-function App() {
-  return <Board />
-}
-export default App
